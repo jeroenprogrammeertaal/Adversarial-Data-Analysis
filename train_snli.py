27f0b37828b1921e3985d739d61a99aa81f9d560
jeroent99 <jeroen1749@gmail.com>
2022-02-08 18:02:49 +0100

    speedup training

--- train_snli.py
+++ train_snli.py
@@ -5,6 +5,7 @@ import torch
 from train_utils import Logger, DataProcessor
 from typing import Union
 from tqdm.auto import tqdm
+from torch.cuda.amp import GradScaler, autocast
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AdamW
 from transformers import get_constant_schedule, get_constant_schedule_with_warmup, get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup
 
@@ -18,9 +19,11 @@ def prepare_model(name:str):
     if name == "bert":
         model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3)
         tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        return model, tokenizer
     if name == "roberta":
         model = AutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=3)
         tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+        return model, tokenizer
 
 class Trainer:
 
@@ -37,6 +40,7 @@ class Trainer:
         self.logger = logger
         self.config = config
         self.optimizer = self.init_optimizer()
+        self.grad_scaler = GradScaler()
         self.lr_scheduler = self.init_lr_scheduler()
         self.device = config["device"]
 
@@ -92,19 +96,32 @@ class Trainer:
     def get_lr(self):
         return self.optimizer.param_groups[0]["lr"]
 
-    def train_step(self, batch):
-        batch = self.to_device(batch)
-        outputs = self.model(**batch)
+    def pre_allocate_memory(self):
+        random_batch = self.data_processor.generate_random_batch()
+        self.model.train()
+        with autocast():
+            outputs = self.model(**random_batch)
         loss = outputs.loss
-        logits = outputs.logits
-        accuracy = self.get_accuracy(logits, batch["labels"])
         loss.backward()
+        self.optimizer.zero_grad(set_to_none=True)
 
-        self.optimizer.step()
+    def train_step(self, batch):
+        self.optimizer.zero_grad(set_to_none=True)
+        batch = self.to_device(batch)
+        
+        with autocast():
+            outputs = self.model(**batch)
+        
+        loss = outputs.loss
+        logits = outputs.logits.detach()
+        accuracy = self.get_accuracy(logits, batch["labels"].detach())
+        
+        self.grad_scaler.scale(loss).backward()
+        self.grad_scaler.step(self.optimizer)
         self.lr_scheduler.step()
-        self.optimizer.zero_grad()
+        self.grad_scaler.update()
         return {
-            "train/loss": loss.item(), 
+            "train/loss": loss.detach().item(), 
             "train/accuracy": accuracy,
             "train/learning_rate": self.get_lr()
         }
@@ -114,6 +131,9 @@ class Trainer:
         validation_loader = self.data_processor.get_dataloader("validation")
         test_loader = self.data_processor.get_dataloader("test")
         progress_bar = tqdm(range(self.get_n_training_steps()))
+
+        if self.config["pre_allocate_memory"]:
+            self.pre_allocate_memory()
         
         for epoch in range(self.config["epochs"]):
             self.model.train()
@@ -136,21 +156,24 @@ class Trainer:
         self.export_weights(epoch)
         self.logger.log_cartography()
 
-    def validation_step(self, batch, split):
+    def validation_step(self, batch, split, epoch):
         idx = batch["idx"]
         batch = self.to_device(batch)
         
-        outputs = self.model(**batch)
-        loss = outputs.loss
-        logits = outputs.logits
+        with autocast():
+            outputs = self.model(**batch)
         
-        accuracy = self.get_accuracy(logits, batch["labels"])
-        correctness = (torch.argmax(logits, dim=-1) == batch["labels"]).int()
-        label_probabilities = self.get_label_probabilities(logits, batch["labels"]).detach()
-        self.logger.store_label_probabilities(idx, label_probabilities, correctness, split)
+        loss = outputs.loss
+        logits = outputs.logits.detach()
+        
+        labels = batch["labels"].detach()
+        accuracy = self.get_accuracy(logits, labels)
+        correctness = (torch.argmax(logits, dim=-1) == labels).int()
+        label_probabilities = self.get_label_probabilities(logits, labels)
+        self.logger.store_label_probabilities(idx, label_probabilities, correctness, split, epoch)
         
         return {
-            "loss": loss.item(), 
+            "loss": loss.detach().item(), 
             "accuracy": accuracy
         }
     
@@ -162,7 +185,7 @@ class Trainer:
         avg_loss = 0
         
         for batch in loader:
-            step_metrics = self.validation_step(batch, split)
+            step_metrics = self.validation_step(batch, split, epoch)
             avg_loss += step_metrics["loss"] / n_batches
             avg_accuracy += step_metrics["accuracy"] / n_batches
         
@@ -213,14 +236,16 @@ if __name__ == "__main__":
                         help="decoupled weight decay")
     parser.add_argument("--warmup_ratio", type=float, default=0.06,
                         help="Fraction of total training step to apply warmup")
+    parser.add_argument("--pre_allocate_memory", action="store_true",
+                        help="Whether to run dummy batches before training to pre-allocate memory.")
 
     # Device
     parser.add_argument("--device", default=torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu"))
-    
     args = vars(parser.parse_args())
     
     model, tokenizer = prepare_model(args["model_name"])
     dataprocessor = DataProcessor(args["dataset_name"], tokenizer, args)
-    logger = Logger(args)
+    split_sizes = {split: dataprocessor.get_split_n_examples(split) for split in ["train", "validation", "test"]}
+    logger = Logger(args, split_sizes)
     trainer = Trainer(model, dataprocessor, logger, args)
     trainer.train()
--- train_utils.py
+++ train_utils.py
@@ -1,9 +1,9 @@
 import os
 import wandb
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle
 
 from collections import defaultdict
 from data_utils.load_configs import LOAD_CONFIGS
@@ -13,11 +13,20 @@ from datasets import concatenate_datasets
 
 class Logger:
 
-    def __init__(self, train_config):
+    def __init__(self, train_config, split_sizes):
         self.config = train_config
         self.best_validation_loss = float("inf")
-        self.label_probabilities = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-
+        self.label_probabilities = {
+            "label_probs": {
+                split: torch.zeros(split_sizes[split], self.config["epochs"], device=self.config["device"], dtype=torch.float16)
+                for split in ["train", "validation", "test"]
+            },
+            "correctness": {
+                split: torch.zeros(split_sizes[split], device=self.config["device"], dtype=torch.float16)
+                for split in ["train", "validation", "test"]
+            }
+        }
+    
         self.init_wandb()
 
     def init_wandb(self):
@@ -38,20 +47,18 @@ class Logger:
     def log(self, metrics: dict):
         wandb.log(metrics)
         if "validation/loss" in metrics.keys():
-            self.update_best_loss(metrics)
+           self.update_best_loss(metrics)
 
     def update_best_loss(self, metrics):
         if self.best_validation_loss > metrics["validation/loss"]:
             self.best_validation_loss = metrics["validation/loss"]
 
-    def store_label_probabilities(self, idx, label_probabilities, correctness, split):
-        for idx, label_prob, correct in zip(idx, label_probabilities, correctness):
-            self.label_probabilities[split][idx.item()]["label_prob"].append(label_prob.item())
-            self.label_probabilities[split][idx.item()]["correctness"].append(correct.item())
+    def store_label_probabilities(self, idx, label_probabilities, correctness, split, epoch):
+        self.label_probabilities["label_probs"][split][idx, epoch] = label_probabilities.squeeze()
+        self.label_probabilities["correctness"][split][idx] += correctness 
 
 
     def log_histogram(self, data, metric, split):
-        # TODO: log to wandb
         plt.hist(data, bins=10, edgecolor="white")
         plt.xlabel(metric)
         plt.ylabel("Number of examples")
@@ -59,27 +66,24 @@ class Logger:
         plt.close()
 
     def log_scatter(self, data, split):
-        sns.scatterplot(data[:, 2], data[:, 1], hue=data[:,3], hue_norm=(0, 1))
+        sns.scatterplot(data[:, 0], data[:, 1], hue=data[:, 2], hue_norm=(0, 1))
         plt.xlabel("Variability")
         plt.ylabel("Confidence")
-        
         wandb.log({f"{split}/variability_confidence_plot": wandb.Image(plt)})
         plt.close()
 
     def log_cartography(self):
-        for split in self.label_probabilities.keys():
-            # Calculate confidences and variabilities
-            data = np.array([
-                [x,
-                np.mean(self.label_probabilities[split][x]["label_prob"]),
-                np.std(self.label_probabilities[split][x]["label_prob"]),
-                np.sum(self.label_probabilities[split][x]["correctness"]) / len(self.label_probabilities[split][x]["correctness"])] 
-                for x in self.label_probabilities[split].keys()
-            ])
+        for split in ["train", "validation", "test"]:
+            data = torch.stack((
+                torch.mean(self.label_probabilities["label_probs"][split], dim=-1),
+                torch.std(self.label_probabilities["label_probs"][split], dim=-1),
+                self.label_probabilities["correctness"][split] / self.config["epochs"]
+            ), dim=1).cpu().numpy()
 
-            self.log_histogram(data[:,1], "confidence", split)
-            self.log_histogram(data[:,2], "variability", split)
-            self.log_histogram(data[:,3], "correctness", split)
+            self.log_histogram(data[:,0], "confidence", split)
+            self.log_histogram(data[:,1], "variability", split)
+            self.log_histogram(data[:,2], "correctness", split)
+            
             self.log_scatter(data, split)
             self.export_cartography(data, split)
 
@@ -94,9 +98,8 @@ class Logger:
         return path
 
     def export_cartography(self, data, split):
-        path = self.get_save_affix() + f"_{split}_cartography.pkl"
-        with open(path, "wb") as f:
-            pickle.dump(data, f)
+        path = self.get_save_affix() + f"_{split}_cartography.pt"
+        torch.save(data, path)
     
 
 
@@ -110,20 +113,25 @@ class DataProcessor:
         self.dataset = self.prepare_dataset()
         self.dataloaders = self.prepare_dataloaders()
 
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
     def tokenize_func(self, examples):
         return self.tokenizer(
             examples["premise"],
             examples["hypothesis"],
             add_special_tokens=True,
-            padding="max_length",
+            padding="longest",
             truncation="only_first"
         )
 
     def prepare_dataset(self):
+        #TODO: tokenize with batch_size=batch_size, shuffle when loading, pad by longest in batch.
         dataset = get_dataset(self.data_config["dataset"], **self.data_config)
+        dataset = dataset.shuffle(self.train_config["seed"])
 
         tokenized_dataset = (
-            dataset.map(self.tokenize_func, batched=True)
+            dataset.map(self.tokenize_func, batched=True, batch_size=self.train_config["batch_size"])
+            .filter(lambda x: 0 <= x["label"] < 3)
             .map(lambda _, idx: {'idx':idx}, with_indices=True)
             .remove_columns(["premise", "hypothesis"])
             .rename_column("label", "labels")
@@ -137,7 +145,7 @@ class DataProcessor:
         return concatenate_datasets([self.dataset[split] for split in splits])
 
     def init_dataloader(self, dataset):
-        return DataLoader(dataset, shuffle=True, batch_size=self.train_config["batch_size"])
+        return DataLoader(dataset, shuffle=False, batch_size=self.train_config["batch_size"], num_workers=4, pin_memory=True)
 
     def prepare_dataloaders(self):
         return {
@@ -146,13 +154,24 @@ class DataProcessor:
             "test": self.init_dataloader(self.get_splits(self.train_config["test_splits"]))
         }
 
+    def generate_random_batch(self, sequence_length=512):
+        random_batch = {}
+        random_batch["input_ids"] = torch.randint(0, 10000, (self.train_config["batch_size"], sequence_length), device=self.train_config["device"])
+        if "token_type_ids" in self.tokenizer.model_input_names:
+            random_batch["token_type_ids"] = torch.zeros(self.train_config["batch_size"], sequence_length, dtype=torch.long, device=self.train_config["device"])
+        if "attention_mask" in self.tokenizer.model_input_names:
+            random_batch["attention_mask"] = torch.ones(self.train_config["batch_size"], sequence_length, dtype=torch.long, device=self.train_config["device"])
+        random_batch["labels"] = torch.zeros(self.train_config["batch_size"], dtype=torch.long, device=self.train_config["device"])
+        return random_batch
+
     def get_dataloader(self, split:str):
         return self.dataloaders[split]
 
     def get_split_size(self, split:str):
         return len(self.dataloaders[split])
 
-
+    def get_split_n_examples(self, split:str):
+        return len(self.dataset[split])
 
 
 
